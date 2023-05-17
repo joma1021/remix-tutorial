1697c4db488938aaca35cbe7570f2fe03150967a
mjost <marius.jost@inovex.de>
2023-05-17 11:49:29 +0200

    add styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,23 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import globalLargeStylesUrl from "~/styles/global-large.css";
+import globalMediumStylesUrl from "~/styles/global-medium.css";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesUrl },
+  {
+    rel: "stylesheet",
+    href: globalMediumStylesUrl,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLargeStylesUrl,
+    media: "screen and (min-width: 1024px)",
+  },
+];
 
 export default function App() {
   return (
@@ -7,6 +26,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,9 @@
-import { Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Link, Outlet } from "@remix-run/react";
+
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesUrl }];
 
 export default function Jokes() {
   return (

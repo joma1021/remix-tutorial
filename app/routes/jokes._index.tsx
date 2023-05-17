d01cc5511867cbaf9cd017a064f3696f41bbfed2
mjost <marius.jost@inovex.de>
2023-05-17 12:43:47 +0200

    add db

--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,8 +1,29 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [joke] = await db.joke.findMany({
+    skip: randomRowNumber,
+    take: 1,
+  });
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+  return json({ joke });
+};
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>Here's your random joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">"{data.joke.name}" Permalink</Link>
     </div>
   );
 }
